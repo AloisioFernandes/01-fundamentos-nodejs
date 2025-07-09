2986d9f80a855b4ad0a83e3ad84d1219b662506c
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-07-09 07:33:34 -0300

    fix: update OneToHundredStream to stop at 5 and improve HTTP server response handling

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -13,7 +13,19 @@ class InverseNumberStream extends Transform {
 
 // req => Readable stream
 // res => Writable stream
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req,res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk); 
+  }
+
+  const fullStramContent = Buffer.concat(buffers).toString();
+
+  console.log(`Full stream content: ${fullStramContent}`);
+
+  return res.end(fullStramContent); // Send the full content back as response
+  
   return req
     .pipe(new InverseNumberStream()) // Transform the stream to inverse the numbers
     .pipe(res); // Pipe the stream to the response
